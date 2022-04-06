b2f3b1a2548aed32aab5d56705d731498851c127
muhammadarash1997 <muhammadarash1997@gmail.com>
2022-04-06 16:04:42 +0800

    second commit

--- castingOrAssertion6.go
+++ castingOrAssertion6.go
@@ -19,15 +19,12 @@ func (a A) Close() error {
 type B struct{}
 
 var _ io.ReadCloser = (*B)(nil)	// <-- Dengan menggunakan ini kita jadi tahu bahwa sebuah struct mengimplementasikan sebuah interface
-
 func New2() B {
 	return B{}
 }
-
 func (b B) Read(p []byte) (n int, err error) {
 	panic("implement me")
 }
-
 func (b B) Close() error {
 	panic("implement me")
 }
