832efe043f8fff537a27b0ca628faaf9ada3074f
muhammadarash1997 <muhammadarash1997@gmail.com>
2022-06-20 20:59:16 +0800

    add closure code

---
+++ Closure/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "joko"
+	counter := 0
+
+	increment := func() {
+		fmt.Println("Increment")
+		name := "joki"	// this variable can't e accessed from outside of the increment
+		counter++
+		fmt.Println(name)
+	}
+
+	increment()
+
+	fmt.Println(counter, name)
+}
