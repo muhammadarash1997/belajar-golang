cf1e8e960869677b41a835a6173502847edf9b17
muhammadarash1997 <muhammadarash1997@gmail.com>
2022-08-01 13:47:54 +0700

    Add some code in random

--- Random/main.go
+++ Random/main.go
@@ -6,5 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(rand.Intn(80)) // return random between 0 until 80
+	fmt.Println(rand.Intn(80))   // return random between 0 until 80
+	fmt.Println(rand.Intn(1e6))  // return random between 0 until 1*10^6 atau 1000000
+	fmt.Println(rand.Intn(10e6)) // return random between 0 until 10*10^6 atau 10000000
 }
